14fb64a91ee0c00ee19819328db6112fed7ee26c
fredegd <124848643+fredegd@users.noreply.github.com>
2023-06-27 19:16:06 +0200

    basic routes

--- pokemonRouter.js
+++ pokemonRouter.js
@@ -1,37 +1,39 @@
-const express = require("express")
+const express = require("express");
 
-let jsonData = require("./pokemon.json")
-
+let jsonData = require("./pokemon.json");
 
 pokemonRouter = express();
 
 //console.log(jsonData)
 
-const getPokemonS = (req, res)=>{
-    res.send(jsonData)
-}
+const getPokemonS = (req, res) => {
+  res.send(jsonData);
+};
+
+const getPokemon = (req, res) => {
+  const { id } = req.params;
+  res.send(jsonData[id - 1]);
+};
 
 
-
-
-
-const getPokemon = (req, res)=>{
-    const { id } = req.params;
-    res.send(jsonData[id-1])
-}
-
-
-
-const getPokemonInfo = (req, res)=>{
-    const { id } = req.params;
-
-const info =req.params.info;    
-
- console.log(info)
-
- res.send(jsonData[id-1].info)
-}
-
+const getPokemonInfo = (req, res) => {
+    const { id, info } = req.params;
+    const pokemon = jsonData[id - 1];
+    console.log(pokemon)
+    if (info === "name") {
+        console.log(pokemon.name)
+      res.send(pokemon.name);
+    } else  if (info === "base") {
+        console.log(pokemon.base)
+      res.send(pokemon.base);
+    } else if (info === "type") {
+        console.log(pokemon.type)
+      res.send(pokemon.type);
+    } else {
+      // Handle other info values here, if needed
+      res.status(404).send("Invalid info parameter");
+    }
+  };
 
 pokemonRouter.get("/", getPokemonS);
 
